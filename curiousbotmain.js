4302207feea05847f2c84a1e5184b56ba579c2f7
Alan <35471486+curiousdoge@users.noreply.github.com>
2020-03-06 18:58:53 -0500

    Create curiousbotmain.js

---
+++ curiousbotmain.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const mr = require('./markovread.js');
+const {prefix,token} = require('./config.json');
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+client.on('ready', () => {console.log("Logged in as "+ client.user.tag); });
+client.on('message', msg => {
+    //if(msg.author.id === "144948237621657601")
+    //mr.mainadder(msg.content, "maddy");
+
+    if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+    const args = msg.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    try{
+        client.commands.get(command).execute(msg, args);
+    } catch(error){
+        console.error(error);
+        msg.reply("The command returned an error: " + error);
+    } });
+
+
+client.login(token);
