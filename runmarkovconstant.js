5ea6d5cddd3c001d0688250e1fc0010a8ea5e7f9
Alan <35471486+curiousdoge@users.noreply.github.com>
2020-03-15 17:39:00 -0400

    Update runmarkovconstant.js

--- runmarkovconstant.js
+++ runmarkovconstant.js
@@ -2,10 +2,10 @@ const mr = require('./markovread.js');
 
 function themessage(anid, achannel, msgcontent){
     mr.mainadder(msgcontent, "<person>");
-    if(anid === "<id>" && !(achannel == ("<channel>" || "<channel>""))){
+    if(anid === "<id>" && !(achannel == "<channel>") && !(achannel == "<channel>") && !(achannel == "<channel>")){
         mr.mainadder(msgcontent, "maddy");
     }
-    if(anid === "<id>" && !(achannel == ("<channel>"" || "<channel>""))){
+    if(anid === "<id>" && !(achannel == "<channel>") && !(achannel == "<channel>") && !(achannel == "<channel>")){
         mr.mainadder(msgcontent, "<person>");
     }
     if(anid === "<id>"){
