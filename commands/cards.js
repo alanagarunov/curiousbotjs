1ec8b6ef7ae9aa364580d96e22af106e0226b036
Alan <35471486+curiousdoge@users.noreply.github.com>
2020-03-15 17:35:44 -0400

    Update cards.js

--- commands/cards.js
+++ commands/cards.js
@@ -1,6 +1,6 @@
 module.exports = {
-    name: 'card',
-    description: 'Displays love live cards',
+    name: 'scout1',
+    description: 'Displays love live cards from love live SIF All Stars \nUSAGE: uwu starsscout OR uwu starsscout <number_of_card>',
     execute(message, args){
         const axios = require('axios');
         const cheerio = require('cheerio');
@@ -8,36 +8,54 @@ module.exports = {
             return Math.floor(Math.random() * (max - min ) ) + min;
         }
 
-        
-        if(args[0] === "1"){
-            const urlbeginning = 'https://schoolido.lu/cards/';
-            const maxnumberofcards = getRandomInt(0,2307);
-            const fullurl = urlbeginning + maxnumberofcards;
-            const newdata = axios.get(fullurl).then(response => {
+        function getcard(){
+            const baseurl = 'http://kachagain.com/sifas/cards.php';
+            const urlbeginning = 'https://kachagain.com/sifas/images/cards/';
+            var maxnumberofcards = getRandomInt(0,190);
+            var fullurl = urlbeginning + maxnumberofcards;
+            //message.channel.send(fullurl);
+            const newdata = axios.get(baseurl).then(response => {
                 const $ = cheerio.load(response.data);
-                const halffinalmessage = $('meta[property="og:image"]').attr('content');
-                const fullfinalmessage = "https:" + halffinalmessage;
-                message.channel.send(fullfinalmessage);
-            });
-        } else {
-            function getcard(){
-                const urlbeginning = 'https://schoolido.lu/cards/';
-                var maxnumberofcards = getRandomInt(0,2308);
-                var fullurl = urlbeginning + maxnumberofcards;
-                return axios.get(fullurl).then(response =>{
-                    return response.data
-                });
-            }
-            for(var x = 0; x < 9; x++){
-                getcard().then(data => {
-                    var $ = cheerio.load(data);
-                    var titleofcard = $('title').text();
-                    var arrayoftitle = titleofcard.split(" ");
-                    //console.log(titleofcard);
-                    message.channel.send(arrayoftitle[6] + " " + arrayoftitle[7] + " " + arrayoftitle[8] + " " + arrayoftitle[9]);
-                });
-            }
+                var yourint = 0;
+                if(args[0] == "view"){
+                    yourint = args[1]
+                } else {
+                    yourint = getRandomInt(0,190)
+                }
 
+                var imageurl = "";
+                var createselector = '#cards > div:nth-child(' + yourint + ') > table > tbody > tr:nth-child(1) > th'
+                var fullurl = urlbeginning + yourint;
+                var title = $(createselector).text()
+                if(title.includes("R")){
+                    imageurl = 'https://media.discordapp.net/attachments/557010696844345344/688535766728441869/0.png'
+                }
+                if(title.includes("SR")){
+                    imageurl = 'https://media.discordapp.net/attachments/557010696844345344/688535583382962369/1.png'
+                }
+                if(title.includes("UR")){
+                    imageurl = 'https://media.discordapp.net/attachments/557010696844345344/688535676127281259/2.png'
+                }
+                const embed = {
+                    "title": title,
+                    "description": "",
+                    "url": "",
+                    "color": 10631964,
+                    "footer": {
+                        "icon_url": "",
+                        "text": "Questions? Comments? Concerns? Me too, pal. -Anna 2020"
+                      },
+                    "thumbnail": {
+                        "url": imageurl
+                      },
+                    "image": {
+                        "url": fullurl
+                      },
+                  };
+                  message.channel.send("", { embed });
+                });
         }
+        getcard();
+
     }
 }
